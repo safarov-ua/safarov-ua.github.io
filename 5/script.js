61ce1e2d321cb046ccfc7c3433120851af8d06b6
Safarov Aleksandr <safarov.ua@gmail.com>
2017-08-31 21:06:57 +0300

    Canvas

--- 5/script.js
+++ 5/script.js
@@ -1,90 +1,81 @@
-let canvas= document.getElementById("canvas");
-		let ctx = canvas.getContext("2d");
-		let color = document.getElementById("color");
-		let size = document.getElementById("size");
-		let clear  =document.getElementById("clear");
-		let arrCircles = [];
-		let circlesDeleted = [];
-		let backgroundColor = document.getElementById("background");
-		let mirrorEffect = document.getElementById("check");
-		let increase = document.getElementById("increase");
-		let decrease = document.getElementById("decrease");
-		let backward = document.getElementById("backward");
-		let forward = document.getElementById("forward");
-		let save = document.getElementById("save");
-		let load = document.getElementById("load");	
-		let localStorageText = document.getElementById("saved");
-		ctx.strokeStyle = color.value;
+window.onload = function(){
+	let canvas= document.getElementById("canvas");
+	let ctx = canvas.getContext("2d");
+	let color = document.getElementById("color");
+	let size = document.getElementById("size");
+	let clear  =document.getElementById("clear");
+	let arrCircles = [];
+	let circlesDeleted = [];
+	let backgroundColor = document.getElementById("background");
+	let mirrorEffect = document.getElementById("check");
+	let increase = document.getElementById("increase");
+	let decrease = document.getElementById("decrease");
+	let backward = document.getElementById("backward");
+	let forward = document.getElementById("forward");
+	let save = document.getElementById("save");
+	let load = document.getElementById("load");	
+	let localStorageText = document.getElementById("saved");
+	ctx.strokeStyle = color.value;
+	
 
-		function toggle(elem, text){
-			elem.innerHTML = text;
-			elem.classList.add("storage-text-visible");
-			setTimeout(function(){
-                	elem.classList.remove("storage-text-visible");
-                	elem.innerHTML = "";
-                }, 2000);
-		}	
-
-		class Circle{
-			constructor(x,y,color,size,scale, mirror){
-				this.x = x;
-				this.y = y;
-				this.color = color;
-				this.size = size;
-				this.scale = scale;
-				this.mirror = mirror;
-				arrCircles.push(this);
-			}
-				draw(){
-       				ctx.beginPath();
-        			ctx.arc(this.x,this.y,this.size*this.scale,0,2*Math.PI);
-        			ctx.fillStyle=this.color;
-        			ctx.fill();
-        			if(this.mirror == true){
-        				ctx.beginPath();
-        				ctx.arc(canvas.width - this.x,this.y,this.size*this.scale,0,2*Math.PI);
-        				ctx.fillStyle=this.color;
-        				ctx.fill();
-        				ctx.beginPath();
-        				ctx.arc(this.x,canvas.height - this.y,this.size*this.scale,0,2*Math.PI);
-        				ctx.fillStyle=this.color;
-        				ctx.fill();
-        				ctx.beginPath();
-        				ctx.arc(canvas.width - this.x,canvas.height -this.y,this.size*this.scale,0,2*Math.PI);
-        				ctx.fillStyle=this.color;
-        				ctx.fill();
-
-        			}
-        			
-    			}
-
-    			
+	class Circle{
+		constructor(x,y,color,size,scale, mirror){
+			this.x = x;
+			this.y = y;
+			this.color = color;
+			this.size = size;
+			this.scale = scale;
+			this.mirror = mirror;
+			arrCircles.push(this);
 		}
+		draw(){
+       		ctx.beginPath();
+        	ctx.arc(this.x,this.y,this.size*this.scale,0,2*Math.PI);
+        	ctx.fillStyle=this.color;
+        	ctx.fill();
+        	if(this.mirror == true){
+        		ctx.beginPath();
+        		ctx.arc(canvas.width - this.x,this.y,this.size*this.scale,0,2*Math.PI);
+        		ctx.fillStyle=this.color;
+        		ctx.fill();
+        		ctx.beginPath();
+        		ctx.arc(this.x,canvas.height - this.y,this.size*this.scale,0,2*Math.PI);
+        		ctx.fillStyle=this.color;
+        		ctx.fill();
+        		ctx.beginPath();
+        		ctx.arc(canvas.width - this.x,canvas.height -this.y,this.size*this.scale,0,2*Math.PI);
+        		ctx.fillStyle=this.color;
+        		ctx.fill();
+        	}	
+    	}    			
+	}
 
-   		/*---mouse position---*/
-   		canvas.onmousemove = function(evt){
+   	/*---Mouse position and draw---*/
+   	canvas.onmousemove = function(evt){
    		
-   			let rect = canvas.getBoundingClientRect();
-			let x = event.clientX - rect.left;
-			let y = event.clientY - rect.top;
+   		let rect = canvas.getBoundingClientRect();
+		let x = event.clientX - rect.left;
+		let y = event.clientY - rect.top;
 
-    		if (evt.buttons) {
-    			circlesDeleted = [];
-    			let circle = new Circle(x, y,color.value, size.value,1,mirrorEffect.checked);
-        		circle.draw();  	 
-    		}
-		}
+    	if (evt.buttons) {
+    		circlesDeleted = [];
+    		let circle = new Circle(x, y,color.value, size.value,1,mirrorEffect.checked);
+        	circle.draw();  	 
+    	}
+	}
 
 	/*---Change canvas background---*/
 	backgroundColor.onchange = function(){
 		canvas.style.backgroundColor = backgroundColor.value;
 	}
+
 	/*---Clear canvas---*/
 	clear.onclick = function(){
 		ctx.clearRect(0,0,canvas.width,canvas.height);
 		arrCircles = [];
 		circlesDeleted = [];
 	}
+
 	/*---Steps actions---*/
 	backward.onclick = function(){
 		ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -112,6 +103,8 @@ let canvas= document.getElementById("canvas");
 			}
 		}
 	}
+
+	/*--Increase decrease---*/
 	increase.onclick = function(){			
 		ctx.clearRect(0,0,canvas.width,canvas.height);
 
@@ -132,6 +125,7 @@ let canvas= document.getElementById("canvas");
 			}	
 		}
 	}
+
 	/*---Save canvas image---*/
 	save.onclick = function(){
 		if(arrCircles.length > 0){
@@ -143,6 +137,17 @@ let canvas= document.getElementById("canvas");
 			toggle(localStorageText, "Nothing to save");
 		}
 	}
+
+	/*---Toogle text---*/
+	function toggle(elem, text){
+			elem.innerHTML = text;
+			elem.classList.add("storage-text-visible");
+			setTimeout(function(){
+                elem.classList.remove("storage-text-visible");
+                elem.innerHTML = "";
+                }, 2000);
+	}
+
 	/*---Load canvas image---*/
 	load.onclick = function(){
 		let a = (JSON.parse(localStorage.getItem("Circle")));
@@ -159,6 +164,6 @@ let canvas= document.getElementById("canvas");
 		else{
 			toggle(localStorageText, "Nothing to load");
 		}
-		localStorage.clear();
-			
+		localStorage.clear();			
 	}
+}
