d0b26f05b60fa53ee7d46ccbe1092dfcf9113927
Safarov Aleksandr <safarov.ua@gmail.com>
2017-09-09 22:51:14 +0300

    maket

--- 6/js/script.js
+++ 6/js/script.js
@@ -1,53 +1,57 @@
-var burger = document.querySelector(".burger-menu");
-var mobMenu = document.querySelector(".mobile-nav");
-var cancel = document.querySelector(".cancel")
-burger.onclick = function(){
-	mobMenu.style.left = "0";
-}
-cancel.onclick = function(){
-	mobMenu.style.left = "-240px";
-}
+window.onload = function() {  
+	var burger = document.querySelector(".burger-menu");
+	var mobMenu = document.querySelector(".mobile-nav");
+	var cancel = document.querySelector(".cancel");
 
-$('.comments-carousel').slick({
-  dots: true,
-  arrows: false,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-      }
-    }
-  ]
-});
+	burger.onclick = function(){
+		mobMenu.style.left = "0";
+	}
 
-$('.future-carousel').slick({
-  dots: true,
-  arrows: false,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-      }
-    }
-  ]
-});
+	cancel.onclick = function(){
+		mobMenu.style.left = "-240px";
+	}
+
+	$('.comments-carousel').slick({
+  		dots: true,
+  		arrows: false,
+  		slidesToShow: 3,
+  		slidesToScroll: 1,
+  		responsive: [
+    	{
+     		breakpoint: 768,
+      		settings: {
+        		slidesToShow: 2,
+        		slidesToScroll: 2,
+      		}
+    	},
+    	{
+      		breakpoint: 480,
+      		settings: {
+        		slidesToShow: 1,
+      		}
+    	}
+  		]
+	});
+
+	$('.future-carousel').slick({
+  		dots: true,
+  		arrows: false,
+  		slidesToShow: 3,
+  		slidesToScroll: 1,
+  		responsive: [
+    	{
+      	breakpoint: 768,
+      	settings: {
+        	slidesToShow: 2,
+        	slidesToScroll: 1,
+      		}
+    	},
+    	{
+      	breakpoint: 480,
+      	settings: {
+        	slidesToShow: 1,
+      		}
+    	}
+  		]
+	});
+};
