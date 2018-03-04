85d7e288855f1c579dc951684eec3323ae3f1017
Safarov Aleksandr <safarov.ua@gmail.com>
2018-03-04 21:59:54 +0200

    10

---
+++ 10/script.js
@@ -0,0 +1,39 @@
+function check(text){
+	var arr = [];
+  var last;
+  var curr;
+  if(typeof text != 'string'){
+  	return false
+  }
+
+ 	for(var i = 0;i<text.length;i++){
+    curr = text[i];
+
+ 	  if((curr=='[') || (curr=='{') || (curr=='(')){
+   		arr.push(curr);
+ 	  }
+ 	  else if((curr==']') || (curr=='}') || (curr==')')){
+ 		  if(arr.length>0){
+        last = arr.pop();
+ 			  if((curr==']' && last!='[') || 
+           (curr=='}' && last!='{') || 
+           (curr==')' && last!='(')){
+   		 		return false
+ 			  }
+ 	    }
+ 	    else{   		
+ 		    return false
+ 	    }
+    }
+  }
+
+  return(arr.length == 0);
+}
+
+var input = document.getElementById("input");
+var button = document.getElementById("check");
+var result = document.getElementById("result");
+
+button.onclick = function(){
+  result.textContent = check(input.value)
+}
