ec760b00440e44f0d13487f37de451c853c882d0
Safarov Aleksandr <safarov.ua@gmail.com>
2018-03-04 22:16:59 +0200

    space

--- 10/script.js
+++ 10/script.js
@@ -1,29 +1,30 @@
 function check(text){
-	var arr = [];
+  var arr = [];
   var last;
   var curr;
+  
   if(typeof text != 'string'){
-  	return false
+    return false
   }
 
- 	for(var i = 0;i<text.length;i++){
+  for(var i = 0;i<text.length;i++){
     curr = text[i];
 
- 	  if((curr=='[') || (curr=='{') || (curr=='(')){
-   		arr.push(curr);
- 	  }
- 	  else if((curr==']') || (curr=='}') || (curr==')')){
- 		  if(arr.length>0){
+    if((curr=='[') || (curr=='{') || (curr=='(')){
+      arr.push(curr);
+    }
+    else if((curr==']') || (curr=='}') || (curr==')')){
+      if(arr.length>0){
         last = arr.pop();
- 			  if((curr==']' && last!='[') || 
-           (curr=='}' && last!='{') || 
+        if((curr==']' && last!='[') ||
+           (curr=='}' && last!='{') ||
            (curr==')' && last!='(')){
-   		 		return false
- 			  }
- 	    }
- 	    else{   		
- 		    return false
- 	    }
+          return false
+        }
+      }
+      else{
+        return false
+      }
     }
   }
 
